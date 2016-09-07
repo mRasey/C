fddca9a43e3dd21a48879572a3793fecb97aabf0
mRasey <billywzh@126.com>
2016-09-07 09:25:08 +0800

    Pointer

--- Pointer/main.cpp
+++ Pointer/main.cpp
@@ -1,17 +1,40 @@
 #include <iostream>
 
+#define NumberOf(x) (sizeof(x) / sizeof(x[0]))
+
 using namespace std;
 
-void foo2(void (*func)) {
-    func;
+void foo2(void (*func)(string), string s) {
+    func(s);
 }
 
-void foo() {
-    cout << "foo";
+void foo(string s) {
+    cout << s;
+}
+
+void swap(int& a, int& b) {
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
+
+void func(int* array, void func(int&, int&)) {
+    func(array[0], array[1]);
+}
+
+int inner(int a, int b) {
+    cout << a << " " << b << endl;
+    return a + b;
+}
+
+int (*outter())(int, int) {
+    return inner;
 }
 
 int main() {
-    foo2((void *) foo);
+    int (*a)(int, int) = outter();
+    cout << a(2, 3) << endl;
     return 0;
 }
 
